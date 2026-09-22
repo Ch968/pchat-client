737f9afce6f9b5fc3d0b38d9fc3187066dcf8d43
Christian <79905391+Ch968@users.noreply.github.com>
2026-09-22 21:57:29 +0100

    Update API base URL to Render backend

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'https://pchat-server-quyh.onrender.com/api'
 });
 
 // Add token to every request
